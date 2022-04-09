6f2f8bf9f543a761e15aeeee8e3a44243627ca96
ZAKARIA RABHI <61620962+zrabhi@users.noreply.github.com>
2022-04-09 10:44:15 +0000

    Delete ft_julia.c

--- ft_julia.c
+++
@@ -1,9 +0,0 @@
-#include "fractol.h"
-
-void    ft_julia(t_fractol *data)
-{
-    // printf("hey");
-    data->newRE = (data->x - WIDTH/2.0)*(4.0/WIDTH);
-    data->newIM = (data->y -HIEGHT/2.0)*(4.0/WIDTH);
-
-  }
