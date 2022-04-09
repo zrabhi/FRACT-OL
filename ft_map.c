31021daaeb659f8a2a310d90dbb3775353d57aa0
ZAKARIA RABHI <61620962+zrabhi@users.noreply.github.com>
2022-04-09 10:44:33 +0000

    Delete ft_map.c

--- ft_map.c
+++
@@ -1,9 +0,0 @@
-#include "fractol.h"
-#include "ft_printf/ft_printf.h"
-
-
-void ft_map(t_fractol *data)
-{
-  data->newRE = (data->x - WIDTH / 2.0) * (4.0 / WIDTH);
-  data->newIM = (data->y - HIEGHT/2.0) * (4.0/ HIEGHT);
-}
